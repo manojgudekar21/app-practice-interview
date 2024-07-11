efdcd078de4e78cfcac2d4bd3e8cc1d91ce09d14
manoj gudekar <manojgudekar21@gmail.com>
2024-07-11 19:32:44 +0530

    added recepies in recepie list

--- src/app/recepies/recepie-list/recepie-list.component.ts
+++ src/app/recepies/recepie-list/recepie-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recepie } from 'src/app/shared/recepie.model';
 
 @Component({
   selector: 'app-recepie-list',
@@ -11,5 +12,10 @@ export class RecepieListComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  recepies:Recepie[] = [
+    new Recepie("Chicken Leg Piece","Its so healthy","https://madscookhouse.com/wp-content/uploads/2020/10/Dum-Chicken-Leg-Roast.jpg"),
+    new Recepie("Mutton Leg Piece","It has high protien","https://i.pinimg.com/736x/88/c4/02/88c4023f6a5ef1d1ffe04d6a9590d86e.jpg"),
+  ]
 
 }
---
+++ src/app/shared/recepie.model.ts
@@ -0,0 +1,5 @@
+export class Recepie {
+    constructor(private name: string,
+        private discription: string,
+        private imagePath: string) { }
+}
