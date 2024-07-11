52ad268ad631b789c49eb4beb270aee71568759f
manoj gudekar <manojgudekar21@gmail.com>
2024-07-11 19:19:23 +0530

    added all the basic UI things and added navigation between recepies and shopping-list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecepiesComponent } from './recepies/recepies.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'recepies', pathMatch:'full'},
+  {path:'recepies', component:RecepiesComponent},
+  {path:'shopping-list', component: ShoppingListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecepiesComponent } from './recepies/recepies.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecepieListComponent } from './recepies/recepie-list/recepie-list.component';
+import { RecepieDetailsComponent } from './recepies/recepie-details/recepie-details.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { RecepieItemComponent } from './recepies/recepie-list/recepie-item/recepie-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecepiesComponent,
+    ShoppingListComponent,
+    RecepieListComponent,
+    RecepieDetailsComponent,
+    ShoppingEditComponent,
+    RecepieItemComponent
   ],
   imports: [
     BrowserModule,
